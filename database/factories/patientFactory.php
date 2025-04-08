73bec861b4486a675f73b039fcd714ea578a07bd
SupunMA <supunmadhusanka62@gmail.com>
2025-04-08 20:02:22 +0530

    patient seeds userID

--- database/factories/patientFactory.php
+++ database/factories/patientFactory.php
@@ -13,21 +13,14 @@ class patientFactory extends Factory
      */
     public function definition()
     {
+        static $userId = 8; // Start from 8
+
         return [
-            // Generate a 10-digit mobile number
             'mobile' => $this->faker->unique()->numerify('##########'),
-
-            // Generate a unique date of birth (random past date)
             'dob' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
-
-            // Random gender: 'M' or 'F'
             'gender' => $this->faker->randomElement(['M', 'F']),
-
-            // Generate a random address
             'address' => $this->faker->address(),
-
-            // Starting from userID 3 and incrementing
-            'userID' => $this->faker->unique()->numberBetween(3, 2002), // You can adjust the range
+            'userID' => $userId++,
         ];
     }
 }
