fc2d365590fa33faaa4144c384b29579dfe6ef7a
SupunMA <supunmadhusanka62@gmail.com>
2025-05-14 18:15:54 +0530

    mu profile ui updated

--- resources/views/Users/Admin/Profile/components/myProfileDetails.blade.php
+++ resources/views/Users/Admin/Profile/components/myProfileDetails.blade.php
@@ -1,53 +1,48 @@
 <!-- This form included into addClient Blade -->
 
-<div class="col-lg-8 col-12 ">
-    <div class="box box-primary">
-        <div class="box-header with-border">
+<!-- This form included into addClient Blade -->
+
+<div class="col-lg-8 col-12">
+    <div class="card card-primary">
+        <div class="card-header">
             <h3 class="card-title">Your Details</h3>
         </div>
         <!-- /.card-header -->
-        <!-- form start -->
 
-        <div class="box-body">
-            
-
+        <!-- Form Start -->
+        <div class="card-body">
             {{-- <form action="{{route('admin.updateAdmin')}}" method="post"> --}}
-                @csrf
-                
-                <input type="hidden" name="id" value="{{$client->id}}">
-                
-                
-    {{-- Gender and DOB --}}
-                <div class="row">
-                    <div class="col-lg-6 col-12">
-                        <div class="form-group">
-                        <label >Name</label>
-                        <input type="name" name="name" value="{{$client->name}}" class="form-control" id="name" placeholder="Enter Name">
-                    
-                        </div>
-                    
-                </div>
-    
-                    
-                    <div class="col-lg-6 col-12">
-                        <div class="form-group">
-                            <label >Email Address</label>
-                            <input type="email" class="form-control" name="email" placeholder="Email Address" value="{{$client->email}}">
-                        </div>
+            @csrf
+
+            <input type="hidden" name="id" value="{{ $client->id }}">
+
+            <!-- Name and Email Row -->
+            <div class="row">
+                <!-- Name Field -->
+                <div class="col-lg-6 col-12">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" value="{{ $client->name }}" class="form-control" id="name" placeholder="Enter Name">
                     </div>
-                    
-    
                 </div>
-  
-                    
+
+                <!-- Email Field -->
+                <div class="col-lg-6 col-12">
+                    <div class="form-group">
+                        <label for="email">Email Address</label>
+                        <input type="email" class="form-control" name="email" id="email" placeholder="Email Address" value="{{ $client->email }}">
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+
             {{-- </form> --}}
         </div>
-
-        
-
-
+        <!-- /.card-body -->
     </div>
+    <!-- /.card -->
 </div>
+<!-- /.col -->>
 {{-- End of First Card --}}
 
 @push('specificJs')
--- resources/views/Users/Admin/Profile/components/profilePWD.blade.php
+++ resources/views/Users/Admin/Profile/components/profilePWD.blade.php
@@ -1,60 +1,66 @@
- <!-- This form included into addClient Blade -->
- 
- {{-- Start of Second Card --}}
-
- <div class="col-lg-8 col-12">
-    <div class="box box-primary">
-        <div class="box-header with-border">
+{{-- Start of Second Card --}}
+<div class="col-lg-8 col-12">
+    <div class="card card-primary">
+        <div class="card-header">
             <h3 class="card-title">Change Password</h3>
         </div>
         <!-- /.card-header -->
-        <!-- form start -->
 
-        <div class="box-body">
+        <!-- Form Start -->
+        <div class="card-body">
 
-            <div class="row">
-                <div class="col-lg-6 col-12">
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">New Password</label>
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="new_password" autocomplete="new-password">
+                <!-- New Password and Confirm Password Row -->
+                <div class="row">
+                    <!-- New Password -->
+                    <div class="col-lg-6 col-12">
+                        <div class="form-group">
+                            <label for="new-password">New Password</label>
+                            <input id="new-password" type="password" class="form-control @error('new_password') is-invalid @enderror" name="new_password" placeholder="Enter new password" autocomplete="new-password">
+                            @error('new_password')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <!-- Confirm New Password -->
+                    <div class="col-lg-6 col-12">
+                        <div class="form-group">
+                            <label for="new-password-confirm">Confirm New Password</label>
+                            <input id="new-password-confirm" type="password" class="form-control" name="new_password_confirmation" placeholder="Confirm new password" autocomplete="new-password">
+                        </div>
                     </div>
                 </div>
+                <!-- /.row -->
 
-                <div class="col-lg-6 col-12">
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Confirm the New Password</label>
-                        <input id="password-confirm" type="password" class="form-control" name="new_password_confirmation" autocomplete="new-password">
+                <small class="form-text text-muted">Minimum password length is 8 characters.</small>
+
+                <!-- Current Password Row -->
+                <div class="row mt-4">
+                    <!-- Current Password -->
+                    <div class="col-lg-6 col-12">
+                        <div class="form-group">
+                            <label for="current-password">Current Password</label>
+                            <input id="current-password" type="password" class="form-control @error('current_password') is-invalid @enderror" name="current_password" placeholder="Enter current password" required autocomplete="current-password">
+                            @error('current_password')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <!-- Submit Button -->
+                    <div class="col-lg-6 col-12 d-flex align-items-end">
+                        <div class="form-group w-100">
+                            <button type="submit" class="btn btn-success btn-lg w-100">
+                                <i class="fas fa-save"></i> Save Changes
+                            </button>
+                        </div>
                     </div>
                 </div>
+                <!-- /.row -->
 
-            </div>
-            <small class="form-text text-muted text-left">Minimum Password Length is 8 characters!</small>
-
-            
         </div>
         <!-- /.card-body -->
-
-        <div class="box-footer">
-            <div class="row">
-                <div class="col-lg-6 col-12">
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Current Password</label>
-                        <input id="password-confirm" type="password" class="form-control" name="current_password" required autocomplete="new-password">
-                    </div>
-                </div>
-                <div class="col-lg-6 col-12">
-                    <div class="form-group pull-right">
-                        <small class="form-text text-muted text-right">Please check details again.</small>
-                        <br>
-                        <button type="submit" class="btn btn-success btn-lg float-right"><b>&nbsp; Save All&nbsp;</b>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
-    
+    <!-- /.card -->
 </div>
-
-
 {{-- End of Second Card --}}
