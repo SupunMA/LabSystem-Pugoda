4bcd7ce41b29f00b6836c2532a93e6eb1dcb6284
SupunMA <supunmadhusanka62@gmail.com>
2025-06-02 09:51:38 +0530

    db seed updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,46 +17,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        // User::create([
-        //     'name' => 'Anuththara',
-        //     'email' => 'admin@a.com',
-        //     'password' => Hash::make('12345678'),
-	    //     'role' => 1
-        // ]);
-
-        $this->call([
-            UserSeeder::class,
-            PatientSeeder::class,
+        User::create([
+            'name' => 'horizon',
+            'email' => 'horizonpugoda@gmail.com',
+            'nic' => '123456789v',
+            'password' => Hash::make('12345678'),
+	        'role' => 1
         ]);
 
-        // AvailableTest::create([
-        //     'AvailableTestName' => 'Fasting Blood Sugar',
-        //     'AvailableTestRange' => '70-120',
-        //     'AvailableTestCost' => 1100,
+        // $this->call([
+        //     UserSeeder::class,
+        //     PatientSeeder::class,
         // ]);
 
-        // AvailableTest::create([
-        //     'AvailableTestName' => 'Full Blood Count',
-        //     'AvailableTestRange' => '80-180',
-        //     'AvailableTestCost' => 1000,
-        // ]);
-
-        // AvailableTest::create([
-        //     'AvailableTestName' => 'Lipid Profile',
-        //     'AvailableTestRange' => '80-180',
-        //     'AvailableTestCost' => 1200,
-        // ]);
-
-        // AvailableTest::create([
-        //     'AvailableTestName' => 'Microalbumin (Urine)',
-        //     'AvailableTestRange' => '80-180',
-        //     'AvailableTestCost' => 1200,
-        // ]);
-
-        // AvailableTest::create([
-        //     'AvailableTestName' => 'Urea & Electrolytes',
-        //     'AvailableTestRange' => '80-180',
-        //     'AvailableTestCost' => 800,
-        // ]);
     }
 }
