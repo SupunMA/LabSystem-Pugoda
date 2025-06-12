b3d6663cc7fef0401022c39f37a55204d461653a
SupunMA <supunmadhusanka62@gmail.com>
2025-06-12 15:55:07 +0530

    whatsapp and map btn added

--- resources/views/Home/components/header.blade.php
+++ resources/views/Home/components/header.blade.php
@@ -25,12 +25,12 @@
     <div class="container">
 <h1>Welcome to HORIZON-LAB</h1>
 <h2><b>"Testing Today, Healthier Tomorrow"</b></h2>
-      @if (Route::has('login'))
+    @if (Route::has('login'))
 
         @auth
         <a href="{{ route('admin.home') }}" class="btn-get-started scrollto">My Account</a>
         @else
-        <a href="{{ route('login') }}" class="btn-get-started scrollto">Login</a>
+        <a href="{{ route('login') }}" style="background-color: #e81a1a; color: white; padding: 10px 20px; text-decoration: none; border-radius: 30px; font-family: Arial, sans-serif; font-size: 16px; display: inline-block;" class="btn-get-started scrollto">Login</a>
 
         @if (Route::has('register2'))
         <a href="{{ route('register2') }}" class="btn-services scrollto">Register</a>
@@ -38,6 +38,17 @@
         @endauth
 
     @endif
+    <a href="https://wa.me/94776267627"  style="background-color: #07943b; color: white; padding: 10px 20px; text-decoration: none; border-radius: 30px; font-family: Arial, sans-serif; font-size: 16px; display: inline-block;" target="_blank">
+        <i class="fab fa-whatsapp" style="margin-right: 8px;"></i>
+        WhatsApp
+    </a>
+<br>
+    <a href="https://maps.app.goo.gl/1wbjHTx3F93WNKm36"
+        target="_blank"
+        style="margin-top: 8px;background-color: #1a73e8; color: white; padding: 10px 20px; text-decoration: none; border-radius: 30px; font-family: Arial, sans-serif; font-size: 16px; display: inline-block;">
+        <i class="fas fa-map-marker-alt" style="margin-right: 8px;"></i>
+        Open Location
+    </a>
     </div>
   </section><!-- End Hero -->
 
