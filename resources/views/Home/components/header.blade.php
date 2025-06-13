62184f823d8f3f9ddb3baa26082a8b3ca8f124bd
SupunMA <supunmadhusanka62@gmail.com>
2025-06-13 19:03:49 +0530

    Nab bar and title changed

--- resources/views/Home/components/header.blade.php
+++ resources/views/Home/components/header.blade.php
@@ -1,7 +1,7 @@
 <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="/">HORIZON</a></h1>
+      <h1 class="logo me-auto"><a href="/">HORIZON LAB</a></h1>
 
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
@@ -9,8 +9,8 @@
       <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
           <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto" href="#about">Services</a></li>
+          <li><a class="nav-link scrollto" href="#services">Available Tests</a></li>
           <li><a class="nav-link" href="{{ route('privacy') }}" target="_blank">Privacy Policy</a></li>
 
         </ul>
